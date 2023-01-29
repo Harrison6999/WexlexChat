3e7a49bb2c329fae85a87edb42115aace8e07a3d
Harrison6999 <123668266+Harrison6999@users.noreply.github.com>
2023-01-29 20:46:58 +0600

    yes

---
+++ action_page.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your first name is: <?php echo $_POST["fname"]; ?>
+Your last name is: <?php echo $_POST["lname"]; ?>
+
+</body>
+</html>
